4852cd439db70351ac259212cb15902feeda8dea
Xerathox <joaquinaguirrep125@gmail.com>
2026-06-09 16:44:27 -0500

    Corrección al agacharse
    El personaje ya no se puede levantar cuando hay un Ground encima de el

--- Assets/Scripts/PlayerCrouch.cs
+++ Assets/Scripts/PlayerCrouch.cs
@@ -7,6 +7,12 @@ public class PlayerCrouch : MonoBehaviour
     private Vector2 originalCollOffset; //almacena el valor original del offset del collider del player
     private Vector2 originalCollSize; //almacena el valor original del size del collider del player
 
+    [Header("Crouch")]
+    public float rayCheckOffset; //Distancia con el cual movemoos el punto desde donde se origina el raycast que detecta el techo sobre la cabeza del player 
+    public float rayCheckDistance; //Detemina el tamaño del rayo
+    public LayerMask headCollision;
+
+
     private void Start()
     {
         playerController = GetComponent<PlayerController>();
@@ -25,12 +31,26 @@ public class PlayerCrouch : MonoBehaviour
             playerController.collPlayer.offset = new Vector2(playerController.collPlayer.offset.x, -0.35f); //ajustar el offset del collider para que se mantenga en el suelo
             playerController.collPlayer.size = new Vector2(playerController.collPlayer.size.x, -0.80f); //ajustar el offset del collider para que se mantenga en el suelo
         }
-        else
+        else if (CanStandUp())
         {
             //Volver el tamaño y el offset del collider
             playerController.collPlayer.offset = originalCollOffset; //ajustar el offset del collider para que se mantenga en el suelo
             playerController.collPlayer.size = originalCollSize; //ajustar el offset del collider para que se mantenga en el suelo
         }
     }
-  
+
+    //Método que determina si el personaje puede pparar se cuando está agachado pasando debajo de algo
+    private bool CanStandUp(){
+        Vector2 originPointRay = new Vector2(transform.position.x, transform.position.y) + Vector2.up * rayCheckOffset; //Punto desde donde se origina el raycast, se ajusta con el rayCheckOffset para que esté por encima de la cabeza del player
+        RaycastHit2D hit = Physics2D.Raycast(originPointRay, Vector2.up, rayCheckDistance, headCollision);
+        return hit.collider == null; //Si el raycast no colisiona con nada, entonces el player puede pararse
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Vector2 originPointRay = new Vector2(transform.position.x, transform.position.y) + Vector2.up * rayCheckOffset; //Punto desde donde se origina el raycast, se ajusta con el rayCheckOffset para que esté por encima de la cabeza del playerq
+        Gizmos.DrawRay(originPointRay, Vector2.up * rayCheckDistance);
+    }
+
 }
